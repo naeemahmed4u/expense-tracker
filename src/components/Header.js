5c902b5bfa538dd81a36a5b0d7aa287d3c1cacf9
naeemahmed4u <76744352+naeemahmed4u@users.noreply.github.com>
2021-03-18 11:59:19 +0500

    Expense Tracker

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h2>
+                Expense Tracker from Naeem Ahmed
+            </h2>
+        </div>
+    )
+}
